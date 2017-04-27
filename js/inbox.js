365f79f3ee2c25830aad84dba9d468ac2ca5f7a3
Federico Brigante <bfred-it@users.noreply.github.com>
2017-04-27 18:05:39 +0900

    Run prettier + lebab
    Whitespace changes and ES6ify

--- js/inbox.js
+++ js/inbox.js
@@ -1,22 +1,22 @@
-setInterval(function(){
-        if (document.hidden) {
-            return;
-        }
-        var total_count = document.querySelectorAll('.an').length;
+setInterval(() => {
+    if (document.hidden) {
+        return;
+    }
+    const total_count = document.querySelectorAll('.an').length;
 
-        const menu_item = document.querySelector('.hA[role="menuitem"] [title="Inbox"]');
-        if (!menu_item) {
-            return;
-        }
+    const menu_item = document.querySelector('.hA[role="menuitem"] [title="Inbox"]');
+    if (!menu_item) {
+        return;
+    }
 
-        // TODO find a smarter way to detect if all items are loaded or not
-        const has_more_items = total_count % 25 == 0;
+    // TODO find a smarter way to detect if all items are loaded or not
+    const has_more_items = total_count % 25 == 0;
 
-        let txt = 'Inbox';
-        if( total_count > 0 ) {
-            txt += ` (${total_count}${has_more_items ? '+' : ''})`;
-        }
-        if (menu_item.textContent !== txt) {
-            menu_item.textContent = txt;
-        }
-},1000);
+    let txt = 'Inbox';
+    if (total_count > 0) {
+        txt += ` (${total_count}${has_more_items ? '+' : ''})`;
+    }
+    if (menu_item.textContent !== txt) {
+        menu_item.textContent = txt;
+    }
+}, 1000);
